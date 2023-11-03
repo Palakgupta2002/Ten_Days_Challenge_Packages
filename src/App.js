1cf97227e3844f834fdaabe6adc11f16da365263
Palakgupta2002 <pg948303@gmail.com>
2023-11-03 19:35:15 +0530

    Process

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Weather from './Components/Weather';
+// import Weather from './Components/Weather';
+import Calculator from './Ten_day_challenge/Calculator/Calculator';
 
 function App() {
   return (
     <div >
-    <Weather/>  
+    {/* <Weather/>   */}
+    <Calculator/>
      
     </div>
   );
