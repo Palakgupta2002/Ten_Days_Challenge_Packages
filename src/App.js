21239f4908796aea0963797ee0995b6b14186592
Palakgupta2002 <pg948303@gmail.com>
2023-11-04 22:23:00 +0530

    Netflix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
+import Netflix from './Ten_day_challenge/Netflix/Netflix';
+
 // import Weather from './Components/Weather';
-import Calculator from './Ten_day_challenge/Calculator/Calculator';
+// import Calculator from './Ten_day_challenge/Calculator/Calculator';
 
 function App() {
   return (
     <div >
     {/* <Weather/>   */}
-    <Calculator/>
+    {/* <Calculator/> */}
+    hii
+    <Netflix/>
      
     </div>
   );
